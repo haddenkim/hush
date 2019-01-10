9360491da1df1514667b8ad1aa8326aaef300e09
hadden <haddenkim3@gmail.com>
2019-01-10 17:07:12 -0600

    cleanup

--- src/scene/obj2scene.h
+++ src/scene/obj2scene.h
@@ -18,8 +18,4 @@ private:
 	static void CreateMeshLight(Scene* scene, Mesh* mesh);
 
 	static Texture* CreateTexture(Scene* scene, std::string filename, std::map<const std::string, Texture*>* textures);
-
-	static void PrintInfo(const tinyobj::attrib_t& attrib,
-						  const std::vector<tinyobj::shape_t>& shapes,
-						  const std::vector<tinyobj::material_t>& materials);
 };
